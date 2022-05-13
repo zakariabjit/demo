1fac224bdd567c46f10ab48b62b03a4bf2350d7e
Zakaria Shahed <zakaria.shahed@bjitgroup.com>
2022-05-13 12:19:12 +0600

    Trying to fix for chromedriver.

--- src/test/java/mytest/BaseTest.java
+++ src/test/java/mytest/BaseTest.java
@@ -1,9 +1,11 @@
 package mytest;
 
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.testng.ITestResult;
@@ -26,29 +28,13 @@ public class BaseTest {
 
     @BeforeClass(alwaysRun = true)
     public void classLevelSetup() throws IOException {
-
-    /*    HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        chromePrefs.put("profile.default_content_settings.popups", 0);
-        ChromeOptions options = new ChromeOptions();
-        HashMap<String, Object> chromeOptionsMap = new HashMap<String, Object>();
-        options.setExperimentalOption("prefs", chromePrefs);
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--disable-extensions");
-        options.setCapability(ChromeOptions.CAPABILITY, chromeOptionsMap);
-        options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-        options.setCapability(ChromeOptions.CAPABILITY, options);
-        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        driver = new FirefoxDriver(options);*/
-        driver=new FirefoxDriver();
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
     }
 
     @AfterMethod(alwaysRun = true)
     public void getResult() throws IOException {
-       driver.close();
+
 
     }
     @AfterClass
